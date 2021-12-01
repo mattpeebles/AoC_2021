4cd76a6b15dbd9bf357ed1c3ab64430bb435b77a
Matt <peebles.mm@gmail.com>
2021-11-30 21:43:38 -0800

    solve day 1

---
+++ day01/day01.ts
@@ -0,0 +1,57 @@
+import fs from 'fs';
+import { start } from 'repl';
+const input = fs.readFileSync('day01.txt').toString().split('\n');
+
+function depthAnalyzer(input: string[])
+{
+    let count = 0;
+    
+    for(let i = 1; i < input.length; i++)
+    {
+        const previous = parseInt(input[i - 1]);
+        const current = parseInt(input[i]);
+
+        if(previous < current)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+function resolveWindow(input: string[], startIndex: number, length: number): number[]
+{
+    return input.slice(startIndex, startIndex + length).map(x => parseInt(x));
+}
+
+const sum = (a: number, b: number) => a + b;
+
+function windowDepthAnalyzer(input: string[], windowSize: number)
+{
+    let count = 0;
+    
+    for(let i = 1; i < input.length - 2; i++)
+    {
+        const previousWindow = resolveWindow(input, i - 1, windowSize)
+        const currentWindow = resolveWindow(input, i, windowSize);
+
+        if(previousWindow.length != windowSize || currentWindow.length != windowSize)
+        {
+            break;
+        }
+
+        if(previousWindow.reduce(sum) < currentWindow.reduce(sum))
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+//part one
+console.log(depthAnalyzer(input));
+
+//part two
+console.log(windowDepthAnalyzer(input, 3));
